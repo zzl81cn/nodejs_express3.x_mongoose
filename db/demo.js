1771d9fb07494d2a5f243d5ce4aab0dfd4f5ac2d
zilong.zhou <zzl81cn@163.com>
2016-06-06 11:51:12 +0800

    1.test.

--- db/demo.js
+++ db/demo.js
@@ -1,10 +1,12 @@
 // mongoose 链接
 var mongoose = require('mongoose');
 var db       = mongoose.createConnection('mongodb://127.0.0.1:27017/NodeJS'); 
+
 // 链接错误
 db.on('error', function(error) {
     console.log(error);
 });
+
 // Schema 结构
 var mongooseSchema = new mongoose.Schema({
     username : {type : String, default : '匿名用户'},
